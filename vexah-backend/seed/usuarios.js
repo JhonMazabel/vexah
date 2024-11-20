70875955f5f9d27692c6c438e1c636ed2aae6468
Jose Toquica <jose.herley.2010@gmail.com>
2024-11-20 15:02:56 -0500

    Inyección de Usuarios

--- vexah-backend/seed/usuarios.js
+++ vexah-backend/seed/usuarios.js
@@ -6,6 +6,12 @@ const usuarios = [
         correo: "admin@vexah.com",
         clave: bcrypt.hashSync("12345678", 10),
         rol: "ADMINISTRADOR"
+    },
+    {
+        nombre: "Juan Perez",
+        correo: "juan.perez@vexah.com",
+        clave: bcrypt.hashSync("12345678", 10),
+        rol: "ASESOR_VENTAS"
     }
 ]
 
